bb8fffb226307f6e7837176d97cdff80f1a5f429
Arthemus <arthemus.moreira@gmail.com>
2019-07-17 09:05:53 +1000

    Check user on API.

--- src/config.ts
+++ src/config.ts
@@ -12,3 +12,5 @@ export const dbSchema = process.env.DATABASE_SCHEMA;
 export const dbUsername = process.env.DATABASE_USERNAME;
 export const dbPassword = process.env.DATABASE_PASSWORD;
 export const dbHost = process.env.DATABASE_HOST;
+
+export const USERS_AUTHENTICATION_API_HOST = process.env.USERS_API_HOST || "http://localhost:6001";
--- src/helpers/middlewares/auth-middleware.ts
+++ src/helpers/middlewares/auth-middleware.ts
@@ -1,10 +1,20 @@
 import { NextFunction, Request, Response } from 'express';
 import jwt from 'jsonwebtoken';
+import axios from 'axios';
 
 import AuthenticationTokenMissingException from '../exceptions/AuthenticationTokenMissingException';
 import WrongAuthenticationTokenException from '../exceptions/WrongAuthenticationTokenException';
 
 import Token from '../utils/token';
+
+import * as config from './../../config';
+
+const fetchUserById = async (id: string): Promise<string> => {
+  const { data } = await axios.get(`${config.USERS_AUTHENTICATION_API_HOST}/users/legancy/${id}`);
+  if (data && data.email)
+    return Promise.resolve(data.email);
+  return Promise.reject();
+}
 
 async function authMiddleware(req: Request, _: Response, next: NextFunction) {
   const token = Token.get(req);
@@ -12,6 +22,8 @@ async function authMiddleware(req: Request, _: Response, next: NextFunction) {
     const secret: string = process.env.JWT_SECRET || 'Spacenow';
     try {
       const { id }: any = await jwt.verify(token, secret);
+      const email: string = await fetchUserById(id);
+      console.debug(`User ${email} verified.`);
       req.userIdDecoded = id;
       next();
     } catch (error) {
